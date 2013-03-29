8b4c9cbca371d0b91e46d4d99c281918f864f5a2
ejamesm <jamesm@bluesunsets.com>
2013-03-28 20:58:09 -0400

    mouseball changes

--- lesson08/mouseball/js/main.js
+++ lesson08/mouseball/js/main.js
@@ -1,1 +1,42 @@
-console.log("hello");
+var $document = $(document);
+var $body     = $('body');
+
+var $container = $('<div></div>');
+$body.append($container);
+
+$container.css({
+    'background-color': 'pink',
+    'height':   '100%',
+    'width':    '100%',
+    'position': 'absolute'
+});
+
+
+var $balls     = [];
+var numOfBalls = 100;
+
+for (var i = 0; i < numOfBalls; i++ ) {
+    $ball  = $('<div class="ball"></div>');
+    $container.append($ball);
+    $balls.push($ball);
+}
+
+function moveBallTo($ball, x, y, duration) {
+
+    $ball.animate({
+      "top":  y + "px",
+      "left": x + "px",
+    },{
+        queue: false,
+        duration: duration,
+        easing: 'linear'
+    });
+
+}
+
+$document.on('mousemove', function(e) {
+    //moveBallTo($ball1, e.clientX, e.clientY, 500);
+    for (var i = 0; i < numOfBalls; i++ ) {
+        moveBallTo($balls[i], e.clientX, e.clientY, 100 * (i+1));
+    }
+});
